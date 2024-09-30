c429adb24f36a2dbca6e8e73c80328434324f70b
psymbio <psymbio@gmail.com>
2024-09-30 23:47:44 +0530

    transaction list all

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,13 +1,11 @@
 import TransactionIssue from "../components/TransactionIssue";
 import TransactionListAll from "../components/TransactionListAll";
-import TransactionReturn from "../components/TransactionReturn";
 
 export default function Transactions() {
     return (
         <div>
             <TransactionListAll />
             <TransactionIssue />
-            <TransactionReturn />
         </div>
     );
 }
