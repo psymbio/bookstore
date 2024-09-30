a823bb3c203893a2f7d0724e45822960021d27a8
psymbio <psymbio@gmail.com>
2024-10-01 00:33:21 +0530

    transaction

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,6 +1,7 @@
 import TransactionHistory from "../components/TransactionHistory";
 import TransactionIssue from "../components/TransactionIssue";
 import TransactionListAll from "../components/TransactionListAll";
+import TransactionRent from "../components/TransactionRent";
 
 export default function Transactions() {
     return (
@@ -8,6 +9,7 @@ export default function Transactions() {
             <TransactionListAll />
             <TransactionIssue />
             <TransactionHistory />
+            <TransactionRent />
         </div>
     );
 }
